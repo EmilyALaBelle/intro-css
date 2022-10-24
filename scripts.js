f81a981c263b6e48ea6f69a2a1e115902a2df5a3
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-24 14:59:32 -0400

    connecting

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const family = [
+    {
+        mom: true,
+        name: 'Angela',
+        job: 'Project Manager',
+    },
+    {
+        kidOne: 'Ashley',
+        kidOneSpouse: 'Randy'
+    },
+    {
+        kidTwo: 'Emily'
+    }
+]
+console.log('Hey class this is my family', family)
+doument.getElementById('contact').style.color = 'purple'
+
+const handleColorChange = () => {
+    console.log('changing color')
+}
